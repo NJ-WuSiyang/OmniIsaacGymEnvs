7c711654c7b790d35831f8c42d10930f72209685
NJ-WuSiyang <wu-sy19@mails.tsinghua.edu.cn>
2022-10-18 16:34:58 +0800

    myfranka

--- omniisaacgymenvs/tasks/playground.py
+++ omniisaacgymenvs/tasks/playground.py
@@ -10,8 +10,10 @@
 from omniisaacgymenvs.tasks.base.rl_task import RLTask
 from omniisaacgymenvs.robots.articulations.ur5e import UR5e
 from omniisaacgymenvs.robots.articulations.ur10 import UR10
+from omniisaacgymenvs.robots.articulations.my_franka import MyFranka
 from omniisaacgymenvs.robots.articulations.views.ur5e_view import UR5eView
 from omniisaacgymenvs.robots.articulations.views.ur10_view import UR10View
+from omniisaacgymenvs.robots.articulations.views.my_franka_view import MyFrankaView
 
 from omni.isaac.core.prims import RigidPrim, RigidPrimView
 from omni.isaac.core.utils.prims import get_prim_at_path
@@ -46,6 +48,7 @@ class PlaygroundTask(RLTask):
 
         self._use_ur5e = self._task_cfg["env"]["use_ur5e"]
         self._use_ur10 = self._task_cfg["env"]["use_ur10"]
+        self._use_myfranka = self._task_cfg["env"]["use_myfranka"]
 
         self.distX_offset = 0.04
         self.dt = 1/60.
@@ -62,6 +65,8 @@ class PlaygroundTask(RLTask):
             self.get_ur5e()
         elif self._use_ur10:
             self.get_ur10()
+        elif self._use_myfranka:
+            self.get_myfranka()
 
         super().set_up_scene(scene)
 
@@ -71,9 +76,17 @@ class PlaygroundTask(RLTask):
         elif self._use_ur10:
             self._ur10s = UR10View(prim_paths_expr="/World/envs/.*/ur10", name="ur10_view")
             scene.add(self._ur10s)
+        elif self._use_myfranka:
+            self._myfrankas = MyFrankaView(prim_paths_expr="/World/envs/.*/myfranka", name="myfranka_view")
+            scene.add(self._myfrankas)
 
         self.init_data()
         return
+
+    def get_myfranka(self):
+        myfranka = MyFranka(prim_path=self.default_zero_env_path + "/myfranka", name="myfranka",
+                            usd_path=self._task_cfg["sim"]["myfranka_usd_path"])
+        self._sim_config.apply_articulation_settings("myfranka", get_prim_at_path(myfranka.prim_path), self._sim_config.parse_actor_config("myfranka"))
 
     def get_ur10(self):
         ur10 = UR10(prim_path=self.default_zero_env_path + "/ur10", name="ur10",
@@ -91,6 +104,8 @@ class PlaygroundTask(RLTask):
             self.ur5e_default_dof_pos = torch.zeros(6, device=self._device)
         elif self._use_ur10:
             self.ur10_default_dof_pos = torch.zeros(6, device=self._device)
+        elif self._myfrankas:
+            self.myfranka_default_dof_pos = torch.tensor([math.degrees(x) for x in [0.0, -1.0, 0.0, -2.2, 0.0, 2.4, 0.8]] + [0.02, 0.02], device=self._device)
 
     def get_observations(self) -> dict:
         if self._use_ur5e:
@@ -103,6 +118,12 @@ class PlaygroundTask(RLTask):
             observations = {
                 self._ur10s.name: {
                     "obs_buf": self._ur10s.get_joint_positions(clone=False)
+                }
+            }
+        elif self._use_myfranka:
+            observations = {
+                self._myfrankas.name: {
+                    "obs_buf": self._myfrankas.get_joint_positions(clone=False)
                 }
             }
         return observations
@@ -126,6 +147,12 @@ class PlaygroundTask(RLTask):
             env_ids_int32 = torch.arange(self._ur10s.count, dtype=torch.int32, device=self._device)
 
             self._ur10s.set_joint_position_targets(self.ur10_dof_targets, indices=env_ids_int32)
+        elif self._use_myfranka:
+            targets = self.myfranka_dof_targets + self.dt * self.actions
+            self.myfranka_dof_targets[:] = targets
+            env_ids_int32 = torch.arange(self._myfrankas.count, dtype=torch.int32, device=self._device)
+
+            self._myfrankas.set_joint_position_targets(self.myfranka_dof_targets, indices=env_ids_int32)
 
     def reset_idx(self, env_ids):
         indices = env_ids.to(dtype=torch.int32)
@@ -151,6 +178,16 @@ class PlaygroundTask(RLTask):
             self._ur10s.set_joint_position_targets(self.ur10_dof_targets[env_ids], indices=indices)
             self._ur10s.set_joint_positions(dof_pos, indices=indices)
             self._ur10s.set_joint_velocities(dof_vel, indices=indices)
+        elif self._use_myfranka:
+            pos = self.myfranka_default_dof_pos.unsqueeze(0)
+            dof_pos = torch.zeros((num_indices, self._myfrankas.num_dof), device=self._device)
+            dof_vel = torch.zeros((num_indices, self._myfrankas.num_dof), device=self._device)
+            dof_pos[:, :] = pos
+            self.myfranka_dof_targets[env_ids, :] = pos
+
+            self._myfrankas.set_joint_position_targets(self.myfranka_dof_targets[env_ids], indices=indices)
+            self._myfrankas.set_joint_positions(dof_pos, indices=indices)
+            self._myfrankas.set_joint_velocities(dof_vel, indices=indices)
 
         # bookkeeping
         self.reset_buf[env_ids] = 0
@@ -166,6 +203,11 @@ class PlaygroundTask(RLTask):
             self.num_ur10_dofs = self._ur10s.num_dof
             self.ur10_dof_targets = torch.zeros(
                 (self._num_envs, self.num_ur10_dofs), dtype=torch.float, device=self._device
+            )
+        elif self._use_myfranka:
+            self.num_myfranka_dofs = self._myfrankas.num_dof
+            self.myfranka_dof_targets = torch.zeros(
+                (self._num_envs, self.num_myfranka_dofs), dtype=torch.float, device=self._device
             )
 
         # randomize all envs
